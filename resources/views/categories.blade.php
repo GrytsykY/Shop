55136fee2862a8bec9747eee335f23de62402e15
Yurii Grytsyk <grytsykyura@gmail.com>
2020-01-16 22:39:33 +0200

    Initial commit

---
+++ resources/views/categories.blade.php
@@ -0,0 +1,35 @@
+@extends('master')
+
+@section('content')
+
+    <div class="starter-template">
+        <div class="panel">
+            <a href="http://internet-shop.tmweb.ru/mobiles">
+                <img src="http://internet-shop.tmweb.ru/storage/categories/mobile.jpg">
+                <h2>Мобильные телефоны</h2>
+            </a>
+            <p>
+                В этом разделе вы найдёте самые популярные мобильные телефонамы по отличным ценам!
+            </p>
+        </div>
+        <div class="panel">
+            <a href="http://internet-shop.tmweb.ru/portable">
+                <img src="http://internet-shop.tmweb.ru/storage/categories/portable.jpg">
+                <h2>Портативная техника</h2>
+            </a>
+            <p>
+                Раздел с портативной техникой.
+            </p>
+        </div>
+        <div class="panel">
+            <a href="http://internet-shop.tmweb.ru/appliances">
+                <img src="http://internet-shop.tmweb.ru/storage/categories/appliance.jpg">
+                <h2>Бытовая техника</h2>
+            </a>
+            <p>
+                Раздел с бытовой техникой
+            </p>
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/product.blade.php
@@ -0,0 +1,16 @@
+@extends('master')
+
+@section('content')
+
+    <div class="starter-template">
+        <h1>iPhone 5SE</h1>
+        <h2>Мобильные телефоны</h2>
+        <p>Цена: <b>17221 руб.</b></p>
+        <img src="http://internet-shop.tmweb.ru/storage/products/iphone_5.jpg">
+        <p>Отличный классический iPhone</p>
+        <form action="http://internet-shop.tmweb.ru/basket/add/4" method="POST">
+            <button type="submit" class="btn btn-success" role="button">Добавить в корзину</button>
+            <input type="hidden" name="_token" value="qF8r5HtbzjmOrvqPWD205mtENDZOMV1M2ZWggFwy"></form>
+    </div>
+
+@endsection
